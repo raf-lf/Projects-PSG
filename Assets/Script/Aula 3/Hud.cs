5ad9059db8aca2ca694c45bc58e86c4ac40bb43e
raf.farinha <raf.farinha@outlook.com>
2021-08-26 17:22:44 -0300

    High Score System + VFX Change

--- Assets/Script/Aula 3/Hud.cs
+++ Assets/Script/Aula 3/Hud.cs
@@ -7,21 +7,50 @@ public class Hud : MonoBehaviour
 {
     public static int score;
     public Text scoreText;
+    public Text recordScoreText;
     public Text gameOverText;
 
     private void Start()
     {
         score = 0;
         Birb.birbDeathEvent += GameOver;
+        UpdateRecordScore();
     }
 
     void GameOver()
     {
-        gameOverText.text = "MUREU :'(";
         Birb.birbDeathEvent -= GameOver;
+
+        gameOverText.text = "MUREU :'(";
+
+        UpdateRecordScore();
+
+        if (PlayerPrefs.HasKey("record"))
+        {
+            if (score > PlayerPrefs.GetInt("record")) 
+                PlayerPrefs.SetInt("record", score);
+        }
+        else
+            PlayerPrefs.SetInt("record", score);
 
     }
 
+    void UpdateRecordScore()
+    {
+        if (PlayerPrefs.HasKey("record"))
+        {
+            recordScoreText.text = PlayerPrefs.GetInt("record").ToString();
+        }
+        else 
+            recordScoreText.text = "-";
+
+    }
+
+    public void ClearRecord()
+    {
+        PlayerPrefs.DeleteKey("record");
+        UpdateRecordScore();
+    }
 
     private void Update()
     {
